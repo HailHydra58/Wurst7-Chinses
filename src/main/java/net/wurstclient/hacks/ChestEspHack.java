1b77e40c2ac83adf613cbd2beb301f02091625ca
hydra <hydra@sztus.com>
2023-11-10 18:17:17 +0800

    箱子高亮汉化

--- src/main/java/net/wurstclient/hacks/ChestEspHack.java
+++ src/main/java/net/wurstclient/hacks/ChestEspHack.java
@@ -41,199 +41,190 @@ import net.wurstclient.util.ChunkUtils;
 import net.wurstclient.util.RenderUtils;
 
 public class ChestEspHack extends Hack implements UpdateListener,
-	CameraTransformViewBobbingListener, RenderListener
-{
-	private final EnumSetting<ChestEspStyle> style =
-		new EnumSetting<>("Style", ChestEspStyle.values(), ChestEspStyle.BOXES);
+        CameraTransformViewBobbingListener, RenderListener {
+    private final EnumSetting<ChestEspStyle> style =
+            new EnumSetting<>("Style", ChestEspStyle.values(), ChestEspStyle.BOXES);
 
-	private final ChestEspBlockGroup basicChests = new ChestEspBlockGroup(
-		new ColorSetting("Chest color",
-			"Normal chests will be highlighted in this color.", Color.GREEN),
-		null);
+    private final ChestEspBlockGroup basicChests = new ChestEspBlockGroup(
+            new ColorSetting("普通箱子",
+                    "普通箱子将以此颜色突出显示。", Color.GREEN),
+            null);
 
-	private final ChestEspBlockGroup trapChests = new ChestEspBlockGroup(
-		new ColorSetting("Trap chest color",
-			"Trapped chests will be highlighted in this color.",
-			new Color(0xFF8000)),
-		new CheckboxSetting("Include trap chests", true));
+    private final ChestEspBlockGroup trapChests = new ChestEspBlockGroup(
+            new ColorSetting("陷阱储物箱",
+                    "陷阱箱子将以此颜色突出显示。",
+                    new Color(0xFF8000)),
+            new CheckboxSetting("Include trap chests", true));
 
-	private final ChestEspBlockGroup enderChests = new ChestEspBlockGroup(
-		new ColorSetting("Ender color",
-			"Ender chests will be highlighted in this color.", Color.CYAN),
-		new CheckboxSetting("Include ender chests", true));
+    private final ChestEspBlockGroup enderChests = new ChestEspBlockGroup(
+            new ColorSetting("末影箱",
+                    "末影箱将以此颜色突出显示。", Color.CYAN),
+            new CheckboxSetting("Include ender chests", true));
 
-	private final ChestEspEntityGroup chestCarts =
-		new ChestEspEntityGroup(
-			new ColorSetting("Chest cart color",
-				"Minecarts with chests will be highlighted in this color.",
-				Color.YELLOW),
-			new CheckboxSetting("Include chest carts", true));
+    private final ChestEspEntityGroup chestCarts =
+            new ChestEspEntityGroup(
+                    new ColorSetting("储物箱矿车",
+                            "带有箱子的矿车将以此颜色突出显示。",
+                            Color.YELLOW),
+                    new CheckboxSetting("Include chest carts", true));
 
-	private final ChestEspEntityGroup chestBoats =
-		new ChestEspEntityGroup(
-			new ColorSetting("Chest boat color",
-				"Boats with chests will be highlighted in this color.",
-				Color.YELLOW),
-			new CheckboxSetting("Include chest boats", true));
+    private final ChestEspEntityGroup chestBoats =
+            new ChestEspEntityGroup(
+                    new ColorSetting("储物箱船",
+                            "带有箱子的船只将以此颜色突出显示。",
+                            Color.YELLOW),
+                    new CheckboxSetting("Include chest boats", true));
 
-	private final ChestEspBlockGroup barrels = new ChestEspBlockGroup(
-		new ColorSetting("Barrel color",
-			"Barrels will be highlighted in this color.", Color.GREEN),
-		new CheckboxSetting("Include barrels", true));
+    private final ChestEspBlockGroup barrels = new ChestEspBlockGroup(
+            new ColorSetting("木桶",
+                    "木桶将以此颜色突出显示。", Color.GREEN),
+            new CheckboxSetting("Include barrels", true));
 
-	private final ChestEspBlockGroup shulkerBoxes = new ChestEspBlockGroup(
-		new ColorSetting("Shulker color",
-			"Shulker boxes will be highlighted in this color.", Color.MAGENTA),
-		new CheckboxSetting("Include shulkers", true));
+    private final ChestEspBlockGroup shulkerBoxes = new ChestEspBlockGroup(
+            new ColorSetting("潜影盒",
+                    "潜影盒将以此颜色突出显示。", Color.MAGENTA),
+            new CheckboxSetting("Include shulkers", true));
 
-	private final ChestEspBlockGroup hoppers = new ChestEspBlockGroup(
-		new ColorSetting("Hopper color",
-			"Hoppers will be highlighted in this color.", Color.WHITE),
-		new CheckboxSetting("Include hoppers", false));
+    private final ChestEspBlockGroup hoppers = new ChestEspBlockGroup(
+            new ColorSetting("漏斗",
+                    "漏斗将以此颜色突出显示。", Color.WHITE),
+            new CheckboxSetting("Include hoppers", false));
 
-	private final ChestEspEntityGroup hopperCarts =
-		new ChestEspEntityGroup(
-			new ColorSetting("Hopper cart color",
-				"Minecarts with hoppers will be highlighted in this color.",
-				Color.YELLOW),
-			new CheckboxSetting("Include hopper carts", false));
+    private final ChestEspEntityGroup hopperCarts =
+            new ChestEspEntityGroup(
+                    new ColorSetting("漏斗矿车",
+                            "带有漏斗的矿车将以此颜色突出显示。",
+                            Color.YELLOW),
+                    new CheckboxSetting("Include hopper carts", false));
 
-	private final ChestEspBlockGroup droppers = new ChestEspBlockGroup(
-		new ColorSetting("Dropper color",
-			"Droppers will be highlighted in this color.", Color.WHITE),
-		new CheckboxSetting("Include droppers", false));
+    private final ChestEspBlockGroup droppers = new ChestEspBlockGroup(
+            new ColorSetting("投掷器",
+                    "投掷器将以此颜色突出显示。", Color.WHITE),
+            new CheckboxSetting("Include droppers", false));
 
-	private final ChestEspBlockGroup dispensers = new ChestEspBlockGroup(
-		new ColorSetting("Dispenser color",
-			"Dispensers will be highlighted in this color.",
-			new Color(0xFF8000)),
-		new CheckboxSetting("Include dispensers", false));
+    private final ChestEspBlockGroup dispensers = new ChestEspBlockGroup(
+            new ColorSetting("发射器",
+                    "发射器将以此颜色突出显示。",
+                    new Color(0xFF8000)),
+            new CheckboxSetting("Include dispensers", false));
 
-	private final ChestEspBlockGroup furnaces =
-		new ChestEspBlockGroup(new ColorSetting("Furnace color",
-			"Furnaces, smokers, and blast furnaces will be highlighted in this color.",
-			Color.RED), new CheckboxSetting("Include furnaces", false));
+    private final ChestEspBlockGroup furnaces =
+            new ChestEspBlockGroup(new ColorSetting("熔炉",
+                    "熔炉、烟熏炉和高炉将以此颜色突出显示。",
+                    Color.RED), new CheckboxSetting("Include furnaces", false));
 
-	private final List<ChestEspGroup> groups = Arrays.asList(basicChests,
-		trapChests, enderChests, chestCarts, chestBoats, barrels, shulkerBoxes,
-		hoppers, hopperCarts, droppers, dispensers, furnaces);
+    private final List<ChestEspGroup> groups = Arrays.asList(basicChests,
+            trapChests, enderChests, chestCarts, chestBoats, barrels, shulkerBoxes,
+            hoppers, hopperCarts, droppers, dispensers, furnaces);
 
-	private final List<ChestEspEntityGroup> entityGroups =
-		Arrays.asList(chestCarts, chestBoats, hopperCarts);
+    private final List<ChestEspEntityGroup> entityGroups =
+            Arrays.asList(chestCarts, chestBoats, hopperCarts);
 
-	public ChestEspHack()
-	{
-		super("ChestESP", "高亮箱子");
-		setCategory(Category.RENDER);
+    public ChestEspHack() {
+        super("ChestESP", "高亮箱子");
+        setCategory(Category.RENDER);
 
-		addSetting(style);
-		groups.stream().flatMap(ChestEspGroup::getSettings)
-			.forEach(this::addSetting);
-	}
+        addSetting(style);
+        groups.stream().flatMap(ChestEspGroup::getSettings)
+                .forEach(this::addSetting);
+    }
 
-	@Override
-	protected void onEnable()
-	{
-		EVENTS.add(UpdateListener.class, this);
-		EVENTS.add(CameraTransformViewBobbingListener.class, this);
-		EVENTS.add(RenderListener.class, this);
+    @Override
+    protected void onEnable() {
+        EVENTS.add(UpdateListener.class, this);
+        EVENTS.add(CameraTransformViewBobbingListener.class, this);
+        EVENTS.add(RenderListener.class, this);
 
-		ChestEspRenderer.prepareBuffers();
-	}
+        ChestEspRenderer.prepareBuffers();
+    }
 
-	@Override
-	protected void onDisable()
-	{
-		EVENTS.remove(UpdateListener.class, this);
-		EVENTS.remove(CameraTransformViewBobbingListener.class, this);
-		EVENTS.remove(RenderListener.class, this);
+    @Override
+    protected void onDisable() {
+        EVENTS.remove(UpdateListener.class, this);
+        EVENTS.remove(CameraTransformViewBobbingListener.class, this);
+        EVENTS.remove(RenderListener.class, this);
 
-		groups.forEach(ChestEspGroup::clear);
-		ChestEspRenderer.closeBuffers();
-	}
+        groups.forEach(ChestEspGroup::clear);
+        ChestEspRenderer.closeBuffers();
+    }
 
-	@Override
-	public void onUpdate()
-	{
-		groups.forEach(ChestEspGroup::clear);
+    @Override
+    public void onUpdate() {
+        groups.forEach(ChestEspGroup::clear);
 
-		ArrayList<BlockEntity> blockEntities =
-			ChunkUtils.getLoadedBlockEntities()
-				.collect(Collectors.toCollection(ArrayList::new));
+        ArrayList<BlockEntity> blockEntities =
+                ChunkUtils.getLoadedBlockEntities()
+                        .collect(Collectors.toCollection(ArrayList::new));
 
-		for(BlockEntity blockEntity : blockEntities)
-			if(blockEntity instanceof TrappedChestBlockEntity)
-				trapChests.add(blockEntity);
-			else if(blockEntity instanceof ChestBlockEntity)
-				basicChests.add(blockEntity);
-			else if(blockEntity instanceof EnderChestBlockEntity)
-				enderChests.add(blockEntity);
-			else if(blockEntity instanceof ShulkerBoxBlockEntity)
-				shulkerBoxes.add(blockEntity);
-			else if(blockEntity instanceof BarrelBlockEntity)
-				barrels.add(blockEntity);
-			else if(blockEntity instanceof HopperBlockEntity)
-				hoppers.add(blockEntity);
-			else if(blockEntity instanceof DropperBlockEntity)
-				droppers.add(blockEntity);
-			else if(blockEntity instanceof DispenserBlockEntity)
-				dispensers.add(blockEntity);
-			else if(blockEntity instanceof AbstractFurnaceBlockEntity)
-				furnaces.add(blockEntity);
+        for (BlockEntity blockEntity : blockEntities)
+            if (blockEntity instanceof TrappedChestBlockEntity)
+                trapChests.add(blockEntity);
+            else if (blockEntity instanceof ChestBlockEntity)
+                basicChests.add(blockEntity);
+            else if (blockEntity instanceof EnderChestBlockEntity)
+                enderChests.add(blockEntity);
+            else if (blockEntity instanceof ShulkerBoxBlockEntity)
+                shulkerBoxes.add(blockEntity);
+            else if (blockEntity instanceof BarrelBlockEntity)
+                barrels.add(blockEntity);
+            else if (blockEntity instanceof HopperBlockEntity)
+                hoppers.add(blockEntity);
+            else if (blockEntity instanceof DropperBlockEntity)
+                droppers.add(blockEntity);
+            else if (blockEntity instanceof DispenserBlockEntity)
+                dispensers.add(blockEntity);
+            else if (blockEntity instanceof AbstractFurnaceBlockEntity)
+                furnaces.add(blockEntity);
 
-		for(Entity entity : MC.world.getEntities())
-			if(entity instanceof ChestMinecartEntity)
-				chestCarts.add(entity);
-			else if(entity instanceof HopperMinecartEntity)
-				hopperCarts.add(entity);
-			else if(entity instanceof ChestBoatEntity)
-				chestBoats.add(entity);
-	}
+        for (Entity entity : MC.world.getEntities())
+            if (entity instanceof ChestMinecartEntity)
+                chestCarts.add(entity);
+            else if (entity instanceof HopperMinecartEntity)
+                hopperCarts.add(entity);
+            else if (entity instanceof ChestBoatEntity)
+                chestBoats.add(entity);
+    }
 
-	@Override
-	public void onCameraTransformViewBobbing(
-		CameraTransformViewBobbingEvent event)
-	{
-		if(style.getSelected().hasLines())
-			event.cancel();
-	}
+    @Override
+    public void onCameraTransformViewBobbing(
+            CameraTransformViewBobbingEvent event) {
+        if (style.getSelected().hasLines())
+            event.cancel();
+    }
 
-	@Override
-	public void onRender(MatrixStack matrixStack, float partialTicks)
-	{
-		// GL settings
-		GL11.glEnable(GL11.GL_BLEND);
-		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-		GL11.glEnable(GL11.GL_CULL_FACE);
-		GL11.glDisable(GL11.GL_DEPTH_TEST);
+    @Override
+    public void onRender(MatrixStack matrixStack, float partialTicks) {
+        // GL settings
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
 
-		matrixStack.push();
-		RenderUtils.applyRegionalRenderOffset(matrixStack);
+        matrixStack.push();
+        RenderUtils.applyRegionalRenderOffset(matrixStack);
 
-		entityGroups.stream().filter(ChestEspGroup::isEnabled)
-			.forEach(g -> g.updateBoxes(partialTicks));
+        entityGroups.stream().filter(ChestEspGroup::isEnabled)
+                .forEach(g -> g.updateBoxes(partialTicks));
 
-		ChestEspRenderer espRenderer = new ChestEspRenderer(matrixStack);
+        ChestEspRenderer espRenderer = new ChestEspRenderer(matrixStack);
 
-		if(style.getSelected().hasBoxes())
-		{
-			RenderSystem.setShader(GameRenderer::getPositionProgram);
-			groups.stream().filter(ChestEspGroup::isEnabled)
-				.forEach(espRenderer::renderBoxes);
-		}
+        if (style.getSelected().hasBoxes()) {
+            RenderSystem.setShader(GameRenderer::getPositionProgram);
+            groups.stream().filter(ChestEspGroup::isEnabled)
+                    .forEach(espRenderer::renderBoxes);
+        }
 
-		if(style.getSelected().hasLines())
-		{
-			RenderSystem.setShader(GameRenderer::getPositionProgram);
-			groups.stream().filter(ChestEspGroup::isEnabled)
-				.forEach(espRenderer::renderLines);
-		}
+        if (style.getSelected().hasLines()) {
+            RenderSystem.setShader(GameRenderer::getPositionProgram);
+            groups.stream().filter(ChestEspGroup::isEnabled)
+                    .forEach(espRenderer::renderLines);
+        }
 
-		matrixStack.pop();
+        matrixStack.pop();
 
-		// GL resets
-		RenderSystem.setShaderColor(1, 1, 1, 1);
-		GL11.glEnable(GL11.GL_DEPTH_TEST);
-		GL11.glDisable(GL11.GL_BLEND);
-	}
+        // GL resets
+        RenderSystem.setShaderColor(1, 1, 1, 1);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glDisable(GL11.GL_BLEND);
+    }
 }
